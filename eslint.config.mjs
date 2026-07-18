7b02e0d9a4c705bbaa56774e36e62331af17f4d1
Brad Preston <bap5393@gmail.com>
2026-07-18 16:16:14 -0400

    add eslint and pre-commit hooks

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', '**/migrations/*'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  }),
+);
